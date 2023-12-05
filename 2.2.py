85ee01b4ec06b4a9be21d06af1cd111a62d913d3
NerfMage <mbasombrio10@srrcc.org.uk>
2023-12-05 16:58:28 +0000

    Day 2 part 2

--- 2.2.py
+++ 2.2.py
@@ -1,27 +1,23 @@
 
 f = open('Input2', 'r')
 
-cubes = {
-    'red': 12,
-    'green': 13,
-    'blue': 14
-}
-
 total = 0
 
 for line in f.readlines():
+    cubes = {
+        'red': 0,
+        'green': 0,
+        'blue': 0
+    }
     line = line.rstrip()
-    possible = True
     gameId = int(line[5:line.index(':')])
 
     for group in line.split(';'):
         for key in cubes.keys():
             if key in group:
                 if int(group[group.index(key)-3:group.index(key)]) > cubes[key]:
-                    possible = False
-                    break
+                    cubes[key] = int(group[group.index(key)-3:group.index(key)])
 
-    if possible:
-        total += gameId
+    total += (cubes['red']*cubes['green']*cubes['blue'])
 
 print(total)
